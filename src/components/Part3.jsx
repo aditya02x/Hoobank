3ed1c757211eda629a59bbc27909f7dfc13ea796
Aditya Raj <adityaraj34@icloud.com>
2025-12-12 12:43:21 +0530

    part 3 responsive

--- src/components/Part3.jsx
+++ src/components/Part3.jsx
@@ -3,8 +3,8 @@ import start from '../assets/Star.svg'
 
 const Part3 = () => {
   return (
-    <div className='flex justify-between mt-20 mb-20'>
-        <div className="left justify-center items-center p-10 ml-15  w-1/2">
+    <div className='flex flex-col md:flex-row justify-between mt-20 mb-20'>a
+        <div className="left justify-center items-center p-10 ml-14 w-full  md:w-1/2">
             <h1 className='text-white text-5xl font-bold'>You do the business, <br />
           we'll handle the money.</h1>
          <  p className='text-gray-400 tracking-wider mt-5'>With the right credit card, you can improve your <br /> financial life by building credit, earning rewards and  <br /> saving money. But with hundreds of credit cards <br /> on the market.</p>
@@ -12,7 +12,7 @@ const Part3 = () => {
     Get Started 
      </div>
         </div>
-        <div className="right flex-col items-center gap-3  w-1/2 flex justify-center  ">
+        <div className="right flex-col items-center gap-3  md:w-1/2 w-full flex justify-center  ">
             <div className="one flex justify-center h-1/3 rounded-2xl  p-6 w-10/12 hover:bg-gradient-to-r
 from-slate-600
 via-slate-800
